e742700e1b3cc7925b875fe5820d4d230ef5818e
kboygit <heybisayangdako@gmail.com>
2016-12-24 12:39:10 -0800

    new exercise

---
+++ octo.rb
@@ -0,0 +1,6 @@
+#An "octothorpe" is also called a "pound", "hash", "mesh", or any number of names. Pick the one that makes you chill out.
+
+#An "octothorpe" or a Hash Symbol is stating a comment
+puts "Hello My name is Gwapo"+"\nGwapo Pero Di kayo"
+
+ 
